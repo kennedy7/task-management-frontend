30c9a1bccd73cb3a5e5dbd31dd68ede715136250
unknown <kennedyhillary6@gmail.com>
2023-04-18 21:58:54 +0100

    npm i

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -1,9 +1,12 @@
-import { post } from 'axios';
-import BaseHttpService from './base-http.service';
+import { post } from "axios";
+import BaseHttpService from "./base-http.service";
 
 export default class AuthService extends BaseHttpService {
   async signin(username, password) {
-    const result = await post(`${this.BASE_URL}/auth/signin`, { username, password });
+    const result = await post(`${this.BASE_URL}/auth/signin`, {
+      username,
+      password,
+    });
     const accessToken = result.data.accessToken;
     this.saveToken(accessToken);
     return result.data.username;
